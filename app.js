46cc493a8b85957f4888411591bb048c4eb997cf
Jared <spicejared@gmail.com>
2022-11-06 12:46:35 -0500

    Remove body-parser, it is deprecated and now comes with express. Create post route for compose page, logs input into console.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,7 +11,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
@@ -29,6 +28,13 @@ app.get("/contact", function(req, res) {
 
 app.get("/compose", function(req, res) {
   res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+
+  let input = req.body.postTitle;
+
+  console.log(input);
 });
 
 
