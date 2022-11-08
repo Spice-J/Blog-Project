ee06f77412536801f647bdfb506c1a0be5dd9c6e
Jared <spicejared@gmail.com>
2022-11-08 04:05:35 -0500

    Add variables blogTitle and blogContent to hold user post title and content. Write render to send said info to post.ejs. Set up post.ejs to display content with header and footer included.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const ejs = require("ejs");
-const lodash = require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -49,15 +49,22 @@ app.post("/compose", function(req, res) {
 });
 
 app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = req.params.postName;
+
+  let requestedTitle = _.lowerCase(req.params.postName);
+        requestedTitle = requestedTitle.split(" ").join("");
 
   posts.forEach(function(post) {
-    const storedTitle = post.title;
+    const blogTitle = post.title;
+    const blogContent = post.content;
+
+    let storedTitle = _.lowerCase(post.title);
+          storedTitle = storedTitle.split(" ").join("");
 
       if (storedTitle === requestedTitle) {
-        console.log("match");
-      } else {
-        console.log("no match");
+        res.render("post", { 
+          blogTitle: blogTitle,
+          blogContent: blogContent
+        });
       }
     });
 });
